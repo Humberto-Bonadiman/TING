4b92820209387b5b735901676691c47b3b76c133
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-07-14 17:31:14 -0400

    feat: create function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,22 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    pass
+    result_word = []
+    len_instance = instance.__len__()
+    for index in range(0, len_instance):
+        frequency = []
+        result = instance.search(index)
+        content = txt_importer(result)
+        for line in range(0, len(content)):
+            if word.upper() in content[line].upper():
+                frequency.append({
+                    "linha": line + 1, "conteudo": content[line]
+                })
+
+        if len(frequency) > 0:
+            result_word.append({
+                "palavra": word,
+                "arquivo": result,
+                "ocorrencias": frequency,
+            })
+    return result_word
